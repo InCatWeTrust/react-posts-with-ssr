93db35808670ad457ab318c5a76261f092dc4558
Paul <paulodintsov@gmail.com>
2022-05-05 18:54:54 +0300

    remove test text components

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,6 @@ import { Layout } from "./shared/Layout"
 import { Header } from "./shared/Header"
 import { Content } from "./shared/Content"
 import { CardsList } from "./shared/CardsList"
-import { Text } from './shared/Text'
-import { Break } from "./shared/Break"
-import { EColor } from './shared/constants/enums'
 
 function AppComponent () {
   return (
@@ -15,18 +12,6 @@ function AppComponent () {
       <Header />
       <Content>
         <CardsList />
-        <Break size={20} top />
-        <Text size={20} mobileSize={28} color={EColor.green} bold>
-          Label1
-        </Text>
-        <Break size={8} mobileSize={16} top />
-        <Text size={20}>
-          Label2
-        </Text>
-        <Break size={16} inline />
-        <Text size={20} mobileSize={16} upperCase>
-          Label3
-        </Text>
       </Content>
     </Layout>
   )
